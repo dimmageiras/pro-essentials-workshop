e6e9149121ca83e5ae7b68e66b317d238cac0496
Dimitrios Mageiras <dimitrios.mageiras@itperspectives.ro>
2025-02-11 14:21:46 +0200

    078-solution

--- src/018-unions-and-narrowing/078-destructuring-a-discriminated-tuple.problem.ts
+++ src/018-unions-and-narrowing/078-destructuring-a-discriminated-tuple.problem.ts
@@ -4,11 +4,12 @@ type User = {
   id: string;
 };
 
-type ApiResponse = [string, User[] | string];
+type ApiResponse = ["success", User[]] | ["error", string];
 
 async function fetchData(): Promise<ApiResponse> {
   try {
     const response = await fetch("https://api.example.com/data");
+
     if (!response.ok) {
       return [
         "error",
@@ -18,6 +19,7 @@ async function fetchData(): Promise<ApiResponse> {
     }
 
     const data = await response.json();
+
     return ["success", data];
   } catch (error) {
     return ["error", "An error occurred"];
