578b077c156ae18d421dffb16a1513e1af452638
Dimitrios Mageiras <dimitrios.mageiras@itperspectives.ro>
2025-02-12 09:39:02 +0200

    092-solution

---
+++ src/020-objects/092-no-autocomplete-on-omit.problem.ts
@@ -0,0 +1,24 @@
+type OmitStrict<
+  T extends Record<PropertyKey, unknown>,
+  K extends keyof T,
+> = Pick<T, Exclude<keyof T, K>>;
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+// You CAN'T omit properties which don't exist!
+// @ts-expect-error
+type UserWithoutPhoneNumber = OmitStrict<User, "phoneNumber">;
+
+// But you CAN'T pick properties which don't exist
+// @ts-expect-error
+type UserWithOnlyPhoneNumber = Pick<User, "phoneNumber">;
+
+// More information:
+
+// The original discussion to add Omit: https://github.com/microsoft/TypeScript/issues/30455
+// The PR to add Omit: https://github.com/microsoft/TypeScript/pull/30552
+// Final word on discussion: https://github.com/microsoft/TypeScript/issues/30825#issuecomment-523668235
