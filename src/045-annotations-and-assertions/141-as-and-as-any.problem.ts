a940aae1f782db3dbabb1c68fcbe368be6b9eaad
Dimitrios Mageiras <dimitrios.mageiras@itperspectives.ro>
2025-02-17 16:10:16 +0200

    141-solution

--- src/045-annotations-and-assertions/141-as-and-as-any.problem.ts
+++ src/045-annotations-and-assertions/141-as-and-as-any.problem.ts
@@ -2,8 +2,10 @@ import { expect, it } from "vitest";
 
 const handleFormData = (e: SubmitEvent) => {
   e.preventDefault();
-  const data = new FormData(e.target);
+
+  const data = new FormData((e.target as HTMLFormElement) || undefined);
   const value = Object.fromEntries(data.entries());
+
   return value;
 };
 
