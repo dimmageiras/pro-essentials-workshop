656439babbbf6a7bdf15b37bea7376de3cddde7f
Dimitrios Mageiras <dimitrios.mageiras@itperspectives.ro>
2025-02-17 16:26:20 +0200

    146.7-solution

---
+++ src/045-annotations-and-assertions/146.7-satisfies-helps-narrow-literals.problem.ts
@@ -0,0 +1,26 @@
+type ButtonAttributes = {
+  type: "button" | "submit" | "reset";
+};
+
+const modifyButton = (attributes: ButtonAttributes) => {};
+
+const buttonAttributes = {
+  type: "button",
+} satisfies ButtonAttributes;
+
+modifyButton(buttonAttributes);
+
+// Example 2
+
+const modifyButtons = (attributes: ButtonAttributes[]) => {};
+
+const buttonsToChange = [
+  {
+    type: "button",
+  },
+  {
+    type: "submit",
+  },
+] satisfies ButtonAttributes[];
+
+modifyButtons(buttonsToChange);
