276ad34cd81edc634a3dc4b6b1c8fc9d1437d7ea
Dimitrios Mageiras <dimitrios.mageiras@itperspectives.ro>
2025-02-19 19:00:20 +0200

    157-solution

--- src/050-the-weird-parts/157-unions-of-functions.problem.ts
+++ src/050-the-weird-parts/157-unions-of-functions.problem.ts
@@ -1,12 +1,14 @@
 const objOfFunctions = {
   string: (input: string) => input.toUpperCase(),
   number: (input: number) => input.toFixed(2),
-  boolean: (input: boolean) => (input ? "true" : "false"),
+  boolean: (input: boolean) => input,
 };
 
 const format = (input: string | number | boolean) => {
   const inputType = typeof input as "string" | "number" | "boolean";
-  const formatter = objOfFunctions[inputType];
+  const formatter = objOfFunctions[inputType] as (
+    inputValue: typeof input
+  ) => string | boolean;
 
   return formatter(input);
 };
